8355fc3e79c6c48534992f4b4f8bfdef6f5b463e
zarakM <mughalzarak@gmail.com>
2020-07-22 17:54:13 +0500

    docker fle upload completed task 2

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="zarakmughal/house_predictor"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login && \
+docker image tag house:1.0 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
