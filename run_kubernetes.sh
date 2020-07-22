2ab2ac4699c0b508ef3da4debb6da961272d12b7
zarakM <mughalzarak@gmail.com>
2020-07-22 19:41:23 +0500

    deployed th machine learning  API to kubernetes cluster and paste output to both output file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="zarakmughal/house_predictor"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run houseapi --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/microservice 8000:80
